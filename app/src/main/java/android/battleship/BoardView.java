1e945e5fc431274f3c1f1d274452d1dd671e56a1
Joanna Marek <asia.marek1@gmail.com>
2018-09-19 23:28:33 +0000

    Added event listiners to the board view

--- app/src/main/java/android/battleship/BoardView.java
+++ app/src/main/java/android/battleship/BoardView.java
@@ -5,18 +5,25 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.battleship.Board.BOARD_SIZE;
 
 public class BoardView extends View {
     private Board board;
+    private boolean isMine;
 
     private Paint whitePaint;
     private Paint blackPaint;
     private Paint redPaint;
     private Paint greyPaint;
     private Paint transparentPaint;
+
+    private List<BoardListener> listeners = new ArrayList<>();
 
     public BoardView(Context context) {
         super(context);
@@ -37,6 +44,10 @@ public class BoardView extends View {
         this.board = board;
     }
 
+    public void setMine(boolean isMine){
+        this.isMine = isMine;
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -44,8 +55,52 @@ public class BoardView extends View {
         drawBoard(canvas);
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_UP:
+                if(!isMine)
+                    break;
+                Coordinate coordinate = findPlaceOnBoard(e.getX(), e.getY());
+                invalidate();
+                notifyBoardListiners(coordinate);
+        }
+        return true;
+    }
+
+    void addListener(BoardListener listener) {
+        if (!listeners.contains(listener)) {
+            listeners.add(listener);
+        }
+    }
+
+    public void removeListener(BoardListener listener) {
+        listeners.remove(listener);
+    }
+
+    private void notifyBoardListiners(Coordinate coordinate) {
+        for (BoardListener listener : listeners) {
+            listener.onTouch(coordinate);
+        }
+    }
+
+    public interface BoardListener {
+        void onTouch(Coordinate coordinate);
+    }
+
+    private Coordinate findPlaceOnBoard(float x, float y) {
+        float measuredFieldSize = getMeasuredFieldSize();
+        int fieldX = (int) (x / measuredFieldSize);
+        int fieldY = (int) (y / measuredFieldSize);
+        return new Coordinate(fieldX, fieldY);
+    }
+
+    private float getMeasuredFieldSize(){
+        return getMeasuredHeight() / BOARD_SIZE;
+    }
+
     private void drawField(Canvas canvas, Coordinate coordinate, Paint paint) {
-        float measuredFieldSize = getMeasuredHeight() / BOARD_SIZE;
+        float measuredFieldSize = getMeasuredFieldSize();
         int x = coordinate.getX();
         int y = coordinate.getY();
         canvas.drawRect(measuredFieldSize * x, measuredFieldSize * y,
@@ -58,10 +113,10 @@ public class BoardView extends View {
             for(int i = 0; i < BOARD_SIZE; i++){
                 for(int j = 0; j < BOARD_SIZE; j++){
                     Field.FieldStatus fieldStatus = boardFields[i][j].getStatus();
-                    if(fieldStatus == Field.FieldStatus.WATER)
-                        continue;
                     switch (fieldStatus){
-                        case SHIP:  drawField(canvas, new Coordinate(i,j), blackPaint);
+                        case SHIP:  if(isMine){
+                                        drawField(canvas, new Coordinate(i,j), blackPaint);
+                                    }
                                     break;
                         case HIT_SHIP:  drawField(canvas, new Coordinate(i,j), redPaint);
                                         break;
